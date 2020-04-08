4ed65f60b7c9b608990257849b3515178ff7d34a
Stephen Buchanan <stephen.buchanan@random.studio>
2020-04-08 14:14:10 +0200

    tests for linear pair panner

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { exclusiveSpeakerPanner } from "./panners";
+import { exclusiveSpeakerPanner, linearPairsPanner } from "./panners";
 
 describe("panning algorithms", () => {
   describe("exlusive speaker panning", () => {
@@ -67,6 +67,63 @@ describe("panning algorithms", () => {
       expect(levels[0]).toEqual(0);
       expect(levels[1]).toEqual(0);
       expect(levels[2]).toEqual(maxVolume);
+    });
+  });
+
+  describe("linear pairs panning", () => {
+    test("4 speakers; position 1 plays exclusively on speaker #1", () => {
+      const target = 1.0;
+      const speakerCount = 4;
+      const levels = linearPairsPanner(target, speakerCount);
+
+      expect(levels).toHaveLength(speakerCount);
+      expect(levels[0]).toEqual(0);
+      expect(levels[1]).toEqual(1.0);
+      expect(levels[2]).toEqual(0);
+      expect(levels[3]).toEqual(0);
+    });
+    test("4 speakers; position 1.5 plays exactly half on speakers #1, #2", () => {
+      const target = 1.5;
+      const speakerCount = 4;
+      const levels = linearPairsPanner(target, speakerCount);
+
+      expect(levels).toHaveLength(speakerCount);
+      expect(levels[0]).toBeCloseTo(0);
+      expect(levels[1]).toBeCloseTo(0.5);
+      expect(levels[2]).toBeCloseTo(0.5);
+      expect(levels[3]).toBeCloseTo(0);
+    });
+    test("4 speakers; position 0.75 plays proportionally between speakers #0, #1", () => {
+      const target = 0.75;
+      const speakerCount = 4;
+      const levels = linearPairsPanner(target, speakerCount);
+
+      expect(levels).toHaveLength(speakerCount);
+      expect(levels[0]).toBeCloseTo(0.25);
+      expect(levels[1]).toBeCloseTo(0.75);
+      expect(levels[2]).toBeCloseTo(0);
+      expect(levels[3]).toBeCloseTo(0);
+    });
+    test("3 speakers; position 1.9 plays mostly on speaker #2", () => {
+      const target = 1.9;
+      const speakerCount = 3;
+      const levels = linearPairsPanner(target, speakerCount);
+
+      expect(levels).toHaveLength(speakerCount);
+      expect(levels[0]).toBeCloseTo(0);
+      expect(levels[1]).toBeCloseTo(0.1);
+      expect(levels[2]).toBeCloseTo(0.9);
+    });
+    test("3 speakers; position 1.9 plays mostly on speaker #2, but at quarter volume", () => {
+      const target = 1.9;
+      const speakerCount = 3;
+      const maxVolume = 0.25;
+      const levels = linearPairsPanner(target, speakerCount, maxVolume);
+
+      expect(levels).toHaveLength(speakerCount);
+      expect(levels[0]).toBeCloseTo(0);
+      expect(levels[1]).toBeCloseTo(0.1 * 0.25);
+      expect(levels[2]).toBeCloseTo(0.9 * 0.25);
     });
   });
 });
--- src/panners.ts
+++ src/panners.ts
@@ -4,30 +4,38 @@ import { remap } from "./utils";
  * Given a target speaker index (round number) will play sound only on that speaker.
  * Index will be rounded if not a whole number.
  * @param target Speaker index to play on; should be a whole number, but will be rounded if not
- * @param speakersCount How many speakers to use in panning
+ * @param speakerCount How many speakers to use in panning
  * @param maxVolume Maximum volume that will be allowed
  */
 export const exclusiveSpeakerPanner = (
   target: number,
-  speakersCount: number,
+  speakerCount: number,
   maxVolume = 1
 ): number[] => {
-  if (target > speakersCount) {
-    throw Error(`target index ${target} exceeds speaker count ${speakersCount}`);
+  if (target > speakerCount) {
+    throw Error(`target index ${target} exceeds speaker count ${speakerCount}`);
   }
-  let levels = Array(speakersCount).fill(0);
+  let levels = Array(speakerCount).fill(0);
   return levels.map((level, index) => (index === Math.round(target) ? maxVolume : 0));
 };
 
+/**
+ * Given a target position between 0 (first speaker) and speakersCount (i.e. last speaker),
+ * pan volume on the nearest pair of speakers to this position. If a whole number, sound will
+ * only come from speaker corresponding to that index (i.e. same as exlusiveSpeakerPanner).
+ * @param target Position, between 0 and last speaker index
+ * @param speakerCount How many speakers to use in panning
+ * @param maxVolume Maximum volume that will be allowed
+ */
 export const linearPairsPanner = (
-  speakersCount: number,
   target: number,
+  speakerCount: number,
   maxVolume = 1
 ): number[] => {
-  if (target > speakersCount) {
-    throw Error(`target index ${target} exceeds speaker count ${speakersCount}`);
+  if (target > speakerCount) {
+    throw Error(`target index ${target} exceeds speaker count ${speakerCount}`);
   }
-  let levels = Array(speakersCount).fill(0);
+  let levels = Array(speakerCount).fill(0);
 
   const pairIndex = {
     left: Math.floor(target),
