d3b1b1983e70b92ae6300a66f8dd822b2fd1aaf0
Stephen Buchanan <stephen.buchanan@random.studio>
2022-09-09 12:58:36 +0200

    WIP reworking example

--- example/example.ts
+++ example/example.ts
@@ -1,10 +1,12 @@
 import MultiChannelPlayer from "../dist";
 import "./clips/jump.mp3";
 import "./clips/land.mp3";
+import "./clips/ambience.mp3";
 
 export const player = new MultiChannelPlayer(2);
 
 player.loadSamples({
   jump: "/clips/jump.mp3",
-  land: "/clips/land.mp3"
+  land: "/clips/land.mp3",
+  ambience: "/clips/ambience.mp3"
 });
